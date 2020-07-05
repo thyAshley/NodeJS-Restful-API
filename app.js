7eaa1b59bfcd7d524c576ae1ab41cc918d346b27
Ashley <thy.ashley@gmail.com>
2020-07-05 18:05:04 +0800

    added chunk and buffers to write file from website

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/about") {
-    res.write("<h1>About me</h1>");
+  if (req.url === "/") {
+    res.write("<html>");
+    res.write(
+      '<form action="/message" method="POST"><input type="text" name="message"></input><button>Send</button></form>'
+    );
+    res.write("</html>");
     res.end();
+  } else if (req.url === "/message" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      res.end();
+    });
   } else {
-    res.write("<h1>Hello World</h1>");
+    res.write("<h1>Hello World</form>");
     res.end();
   }
 });
