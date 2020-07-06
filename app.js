b78caf874261c19c57fa212e51450d4106ff9069
Ashley <thy.ashley@gmail.com>
2020-07-06 14:28:51 +0800

    added express to project and adding middleware

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
-const { requestHandler } = require("./routes");
-
-const server = http.createServer(requestHandler);
-
-server.listen(3000, () => {
-  console.log("port started on port 3000");
+app.use((req, res, next) => {
+  console.log("in middleware");
+  next();
 });
+
+app.use((req, res, next) => {
+  console.log("in middleware 2");
+  res.send("<h1>Hello from Middleware</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("server started on port 3000");
+});
+
+app.route("/").get((req, res) => {
+  res.end("hello");
+});
+
+// const { requestHandler } = require("./routes");
+
+// const server = http.createServer(requestHandler);
+
+// server.listen(3000, () => {
+//   console.log("port started on port 3000");
+// });
