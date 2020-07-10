9680c9b105d188c4e4b30a7a7694c04fbdda9c62
Ashley <thy.ashley@gmail.com>
2020-07-10 14:20:44 +0800

    added mongodb package and started server to mongodb

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
-const sequelize = require("./util/database");
 const errorController = require("./controllers/errorController");
-const Product = require("./models/productModel");
-const User = require("./models/userModel");
-const Cart = require("./models/cartModel");
-const CartItem = require("./models/cartItemModel");
-const OrderItem = require("./models/orderItemModel");
-const Order = require("./models/orderModel");
-
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const mongoConnect = require("./util/database");
 
 const app = express();
 
@@ -22,58 +16,14 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
-});
-
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.hasMany(OrderItem);
-Order.belongsToMany(Product, { through: OrderItem });
-
-sequelize
-  .sync()
-  .then((result) => {
-    User.findByPk(1)
-      .then((user) => {
-        if (!user) {
-          return User.create({
-            name: "Max",
-            email: "Dummy@test.com",
-          });
-        }
-        return user;
-      })
-      .then((user) => {
-        console.log("User created");
-        return user.createCart();
-      })
-      .then((cart) => {
-        console.log("cart created");
-        app.listen(3000, () => {
-          console.log("server started on port 3000");
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  })
-  .catch((err) => {
-    console.log(err);
+mongoConnect((client) => {
+  console.log(client);
+  app.listen(3000, () => {
+    console.log("Server started at port 3000");
   });
+});
--- util/database.js
+++ util/database.js
@@ -1,8 +1,28 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("node-complete", "root", "test123", {
-  dialect: "mysql",
-  host: "localhost",
-  logging: false,
-});
+// const Sequelize = require("sequelize");
+// const sequelize = new Sequelize("node-complete", "root", "test123", {
+//   dialect: "mysql",
+//   host: "localhost",
+//   logging: false,
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+const mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+
+console.log(process.env.MONGO_PASSWORD);
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    `mongodb+srv://admin-ashley:${process.env.MONGO_PASSWORD}@testdb-ukelm.mongodb.net/<dbname>?retryWrites=true&w=majority`
+  )
+    .then((client) => {
+      console.log("Connected to MongoDB Atlas");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = mongoConnect;
